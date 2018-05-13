7a001d1e7fc3a0acb35bf12e6fcd52d9af991a0e
ignas26 <ignas.zlatkus@gmail.com>
2018-05-14 01:06:32 +0300

    updated index.js with rootReducer, added assets/array/colours.js state, added reducers with coloursReducer.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRoutes from './AppRoutes';
 import 'normalize.css';
 import './sass/main.scss';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import AppRoutes from './AppRoutes';
+import coloursReducer from './reducers/coloursReducer';
 
 
+const rootReducer=combineReducers({colours:coloursReducer});
 
+const store=createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<AppRoutes/>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <AppRoutes/>
+</Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/coloursReducer.js
@@ -0,0 +1,9 @@
+import colourArray from  '../assets/array/colours';
+
+const coloursReducer=(state=colourArray, action)=>{
+  switch(action.type){
+    default:return state;
+  }
+};
+
+export default coloursReducer;
