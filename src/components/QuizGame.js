0857781dfb1e5a1ff5c13ef5d83739f4f4c293b0
ignas26 <ignas.zlatkus@gmail.com>
2018-06-07 14:42:40 +0300

    working single square selection functionality (clickHandler) added

--- src/components/QuizGame.js
+++ src/components/QuizGame.js
@@ -3,82 +3,84 @@ import _ from 'lodash';
 import {connect} from 'react-redux';
 
 
-
 class QuizGame extends React.Component {
-state={
-  squares:[],
-  level:1,
-  color:'',
-  values:[]
-};
+  state = {
+    squares: [],
+    level: 1,
+    color: '',
+    clicks: []
+  };
 
-  componentDidMount () {
+  componentDidMount() {
     this.renderSquare();
   }
 
-
-renderSquare = () => {
-
-  const url = this.props.match.params.colour;
-
-  let activeColour = this.props.variety.filter(item=>{
-    return item.colour===url});
-
-  const r = activeColour[0].values[0];
-  const g = activeColour[0].values[1];
-  const b = activeColour[0].values[2];
+  clickHandler = (i) => {
+    const clicks = [...this.state.clicks];
+    clicks[i] = !clicks[i];
+    this.setState({clicks})
+  };
 
 
-  const newValue = [r, g, b];
+  renderSquare = () => {
+
+    const url = this.props.match.params.colour;
+
+    let activeColour = this.props.variety.filter(item => {
+      return item.colour === url
+    });
+
+    const r = activeColour[0].values[0];
+    const g = activeColour[0].values[1];
+    const b = activeColour[0].values[2];
+
+
+    const newValue = [r, g, b];
+    const clicks = [];
 
     let colours = [];
     for (let i = 0; i < 49; i++) {
-     colours.push(`rgb(${newValue})`)
+      colours.push(`rgb(${newValue})`);
+      clicks.push(false)
     }
 
     for (let i = 50; i < 55; i++) {
-      colours.push(`rgb(${newValue[0]-75/this.state.level},${newValue[1]},${newValue[2]})`)
+      colours.push(`rgb(${newValue[0] + 100 / this.state.level},${newValue[1] + 100 / this.state.level},${newValue[2] + 150 / this.state.level})`);
+      clicks.push(true);
     }
-
-
+    console.log(clicks);
+    console.log(colours);
 
     const shuffle = _.shuffle(colours);
     this.setState({
       squares: shuffle
     });
-  console.log(this.state.squares);
-
   };
 
 
   render() {
 
-    // const url = this.props.match.params.colour;
-    //
-    // let activeColour = this.props.variety.filter(item=>{
-    //     return item.colour===url});
-    //
-    // const r = activeColour[0].values[0];
-    // const g = activeColour[0].values[1];
-    // const b = activeColour[0].values[2];
-
-
-  const squares = this.state.squares.map((square, i)=>{
-    return <div key={i} className="singleBlock" style={{backgroundColor: square}}></div>
-      });
+    const squares = this.state.squares.map((square, i) => {
+      return <div
+          key={i}
+          onClick={() => this.clickHandler(i)}
+          className={this.state.clicks[i]? "activeBlock" : "singleBlock"}
+          style={{backgroundColor: square}}
+      >
+      </div>
+    });
 
     return (
         <div className="allBlocks">
-        {squares}
+          {squares}
         </div>
     )
   }
 }
 
-const mapStateToProps=(state)=>{
-  return{variety:state.variety}
+const mapStateToProps = (state) => {
+  return {variety: state.variety}
 };
-
 
 
 export default connect(mapStateToProps)(QuizGame)
